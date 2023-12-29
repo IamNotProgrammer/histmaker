f996499ea47f34c588001f85edda0402db95cc36
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2023-12-29 16:11:12 +0100

    safdkbj

--- hist.c
+++ hist.c
@@ -3,23 +3,49 @@
 #include <string.h>
 
 
+void help()
+	{
+
+	printf("How to use this thing:\n \
+	a.out [options] file\n\n \
+	OPTIONS:\n \
+	--help\t print this\n \
+	-b\t beginning - where the first bin should be placed on x axis (float), default 0\n \
+	-c\t centering - should output add half step to bin x axis (0 - no, 1 - yes), default 1\n \
+	-n\t number of bins - set number of bins, default 250\n \
+	-s\t step size - how wide are bins, default 1\n\n") ;
+
+	}
+
+
 int main(int argc, char **argv)
 {
 
+if (argc == 1)
+	{
+	printf("File not found\n") ;
+	exit(1) ;
+	}
+
 float f, step, beg, center ; // f - where bin is on x axis, step - number of steps (default 250), beg - see below
 							// center - flag do you want bins shifted by half step default 1
-int n, c ; // n - bin number in array, c - character from file
+int n, c ; // n - bin number in array, c - character from file, arSize - number of bins (default 250)
+unsigned int *pArray, arSize ; // pointer to array, array size
+
 FILE *pFile ;
 
 center = 1 ;
 beg = 0 ; // beginning where histogram starts, default = 0
+arSize = 250 ;
+step = 1 ;
+
 
 for (int i = 1; i < argc; i++) // don't know how else make flags
 	{
 
 	if (strcmp(argv[1], "--help") == 0) // compares strings
 		{
-		printf("help\n") ; // TODO write help
+		help() ;
 		return 0 ;
 		}
 
@@ -35,19 +61,19 @@ for (int i = 1; i < argc; i++) // don't know how else make flags
 		center = strtof(argv[i], NULL) ;
 		}
 
+	if (strcmp(argv[i], "-n") == 0) // number of bins (max 250)
+		{
+		i++ ;
+		arSize = strtof(argv[i], NULL) ;
+		}
+
+	if (strcmp(argv[i], "-s") == 0) // step size
+		{
+		i++ ;
+		step = strtof(argv[i], NULL) ;
+		}
+
 /*	TODO
-	if (strcmp(argv[i], "-c") == 0) // step size
-		{
-		i++ ;
-		center = strtof(argv[i], NULL) ;
-		}
-
-	if (strcmp(argv[i], "-c") == 0) // number of bins (max 250)
-		{
-		i++ ;
-		center = strtof(argv[i], NULL) ;
-		}
-
 	if (strcmp(argv[i], "-c") == 0) // column number because for now file has to be just 1 column of values to divide
 		{
 		i++ ;
@@ -64,19 +90,20 @@ for (int i = 1; i < argc; i++) // don't know how else make flags
 
 	}
 
-unsigned int a1[250] ;
+pArray = (unsigned int *) calloc(arSize, sizeof(unsigned int)) ; // allocate memory
 
-for(int i = 0; i < 250; i++)
-	a1[i] = 0 ;
+if (pArray == NULL)
+	{
+	printf("calloc NULL pointer\nToo many bins!'n") ;
+	exit(1) ;
+	}
 
 pFile = fopen(argv[argc-1], "r") ;
-
-step = 250 ;
 
 if (pFile == NULL)
 	{
 	printf("File not found\n") ;
-	return 1 ;
+	exit(1) ;
 	}
 
 c = fscanf(pFile, "%f", &f) ;
@@ -86,8 +113,8 @@ while (c != EOF)
 	f /= step ;
 	n = (int) f ;
 	
-	if(n < 250)
-		n[a1] ++ ;	
+	if(n < arSize)
+		*(pArray + n) = *(pArray + n) + 1 ;
 
 	c = fscanf(pFile, "%f", &f) ;
 	} 
@@ -96,12 +123,10 @@ fclose(pFile) ;
 
 pFile = fopen("datout.dat", "w") ;
 
-for (int i = 0; i < 250; i++)
-	{
-	if (i < 250)
-		fprintf(pFile, "%f %d\n", beg + step * (float)i + center * 0.5 * step, a1[i]) ;
-	}
+for (int i = 0; i < arSize; i++)
+		fprintf(pFile, "%f %d\n", beg + step * (float)i + center * 0.5 * step, *(pArray+i)) ;
 
+free(pArray) ; // free allocated memory. I don't wanna to find out what happens if I don't
 fclose(pFile) ;
 
 return 0 ;
